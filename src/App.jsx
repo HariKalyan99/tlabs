3d46fcfe8c51708272e14412e67864ca22ef1eb7
Harikalyan99 <harikalyan99tronics@gmail.com>
2024-03-27 11:42:10 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import Slideshow from './components/Slideshow'
+import Header from './components/Header'
+import Dashboard from './components/Dashboard'
+import Jumbotron from './components/Jumbotron'
+import Pricing from './components/Pricing'
+import Footer from './components/Footer'
+import Features from './components/Features'
+
+function App() {
+  return (
+   <div>
+    <Header />
+    <Slideshow />
+    <Jumbotron />
+    <Dashboard />
+    <Pricing />
+    <Features />
+    <Footer />
+   </div>
+  )
+}
+
+export default App
